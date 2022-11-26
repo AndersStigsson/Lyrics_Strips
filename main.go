cdd5061286106a84a3ef40769056195528429474
Anders Stigsson <anders@pixelstore.se>
2022-11-26 15:57:17 +0100

    Move router handling to router file

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"math/rand"
-	"net/http"
 	"os"
-	"strings"
 	"time"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -29,39 +23,6 @@ type SpotifyData struct {
 }
 
 var client SpotifyData
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func validateRequest(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-		}
-		if client.AccessToken.Expires.Before(time.Now()) {
-			// get new accessToken
-		}
-		if client.UserToken.Expires.Before(time.Now()) {
-			// get new UserToken
-		}
-		next(w, r)
-	}
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	// myRouter.HandleFunc("/banan", validateRequest(addNewGuest))
-
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "x-api-token"})
-	// originsOk := handlers.AllowedOrigins([]string{"*"})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-
-	log.Fatal(http.ListenAndServe(":10010", handlers.CORS(headersOk, methodsOk)(myRouter)))
-}
 
 func main() {
 	enverr := godotenv.Load(".env")
@@ -84,24 +45,5 @@ func main() {
 	clientInfo := client.getClientCredentialsLogin()
 	clientInfo.Expires = time.Now().Add(time.Second * time.Duration(clientInfo.ExpiresIn))
 	client.UserToken = clientInfo
-	trackData := client.getTrackId()
-	fmt.Println(client.UserToken.AccessToken)
 	handleRequests()
-
-	numberOfSongs := len(trackData.Tracks)
-	songNumber := rand.Intn(numberOfSongs)
-	lyrics := client.getLyrics(trackData.Tracks[songNumber].Id)
-	lineNumber := rand.Intn(len(lyrics.Lyrics.Lines))
-	lineOfText := lyrics.Lyrics.Lines[lineNumber]
-	for {
-		if len(strings.Split(lineOfText.Words, " ")) > 3 {
-			break
-		}
-		lineNumber = rand.Intn(len(lyrics.Lyrics.Lines))
-		lineOfText = lyrics.Lyrics.Lines[lineNumber]
-	}
-
-	fmt.Println(lineOfText)
-	fmt.Scanln()
-	fmt.Printf("Namn: %s, Artist: %s", trackData.Tracks[songNumber].Name, trackData.Tracks[songNumber].Artist[0].Name)
 }
---
+++ router.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func validateRequest(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
+		}
+		if client.AccessToken.Expires.Before(time.Now()) {
+			client.AccessToken = client.getAccessToken()
+		}
+		if client.UserToken.Expires.Before(time.Now()) {
+			client.UserToken = client.getClientCredentialsLogin()
+		}
+		next(w, r)
+	}
+}
+
+func getNextTrack(w http.ResponseWriter, r *http.Request) {
+	trackData := client.getTrackId()
+	fmt.Println(client.UserToken.AccessToken)
+
+	numberOfSongs := len(trackData.Tracks)
+	songNumber := rand.Intn(numberOfSongs)
+	lyrics := client.getLyrics(trackData.Tracks[songNumber].Id)
+	lineNumber := rand.Intn(len(lyrics.Lyrics.Lines))
+	lineOfText := lyrics.Lyrics.Lines[lineNumber]
+	for {
+		if len(strings.Split(lineOfText.Words, " ")) > 2 {
+			break
+		}
+		lineNumber = rand.Intn(len(lyrics.Lyrics.Lines))
+		lineOfText = lyrics.Lyrics.Lines[lineNumber]
+	}
+	type ReturnData struct {
+		Line  LyricsLines `json:"line"`
+		Track Tracks      `json:"track"`
+	}
+
+	var returnData ReturnData
+	returnData.Line = lineOfText
+	returnData.Track = trackData.Tracks[songNumber]
+
+	json.NewEncoder(w).Encode(returnData)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/next", validateRequest(getNextTrack))
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "x-api-token"})
+	// originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	log.Fatal(http.ListenAndServe(":10010", handlers.CORS(headersOk, methodsOk)(myRouter)))
+}
