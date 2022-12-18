2e532e803cb9680da1b0956f40f97cef256cf9f7
Anders Stigsson <anders@pixelstore.se>
2022-12-18 19:12:52 +0100

    More clear which default genre is

--- frontend/doobidoo/src/Dropdown.vue
+++ frontend/doobidoo/src/Dropdown.vue
@@ -23,7 +23,7 @@ export default {
     ],
     data() {
         return {
-            value: null,
+            value: ['swedish'],
             options: [
                 "acoustic",
                 "afrobeat",
